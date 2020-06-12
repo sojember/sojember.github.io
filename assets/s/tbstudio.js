ff182b07ae56c44dcb6ce882499966dbe5242ec2
sojember <66819949+sojember@users.noreply.github.com>
2020-06-12 19:31:29 +0700

    Add files via upload
    first

---
+++ assets/s/tbstudio.js
@@ -0,0 +1,45 @@
+var unggulan1 = document.getElementById('unggulan1');
+var unggulan2 = document.getElementById('unggulan2');
+var game1 = document.getElementById('game1');
+var game2 = document.getElementById('game2');
+var game3 = document.getElementById('game3');
+var prev = document.getElementById('prev');
+var next = document.getElementById('next');
+var twitter = document.getElementById('twitter');
+
+unggulan1.addEventListener('click', function(){
+    open("https://s.id/soj-terasi", "_blank");
+    // window.open('url here', '_BLANK');
+});
+
+unggulan2.addEventListener('click', function(){
+    open("https://s.id/soj-terasi", "_blank");
+    // window.open('url here', '_BLANK');
+});
+
+game1.addEventListener('click', function(){
+    open("https://s.id/soj-cabejawa", "_blank");
+    // window.open('url here', '_BLANK');
+});
+
+game2.addEventListener('click', function(){
+    open("https://s.id/soj-maskerkain", "_blank");
+    // window.open('url here', '_BLANK');
+});
+
+game3.addEventListener('click', function(){
+    open("https://s.id/soj-wayang", "_blank");
+    // window.open('url here', '_BLANK');
+});
+
+prev.addEventListener('click', function(){
+    alert('belum ada produk lain tersedia');
+});
+
+next.addEventListener('click', function(){
+    alert('belum ada produk lain tersedia');
+});
+
+twitter.addEventListener('click', function(){
+    alert('media ini belum tersedia');
+});
---
+++ index.php
@@ -0,0 +1,28 @@
+<head>
+<title>TB Studio - Next Gen's Vision</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="https://fonts.googleapis.com/css?family=Chakra+Petch|Oxanium|Roboto+Slab|Source+Sans+Pro&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="assets/s/tbstudio.css">
+
+</head>
+<body>
+<div class="nav">
+<?php include('assets/m/navbar.html'); ?>
+</div>
+<div class="page" id="page1">
+<?php include('assets/p/page1.html'); ?>
+</div>
+<div class="page" id="page2">
+<?php include('assets/p/page2.html'); ?>
+</div>
+<div class="page" id="page3">
+<?php include('assets/p/page3.html'); ?>
+</div>
+<div class="page" id="page4">
+<?php include('assets/p/page4.html'); ?>
+</div>
+<div class="footer">
+<?php include('assets/p/footer.html'); ?>
+</div>
+<script src="assets/s/tbstudio.js"></script>
+</body>
